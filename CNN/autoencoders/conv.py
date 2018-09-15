202abde20885564684d4558bd721cdf26624ba79
Sirebellum <joshua.herrera227@gmail.com>
2018-09-15 03:39:09 -0500

    Various cnn related files, semi working

---
+++ CNN/autoencoders/conv.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+
+def encode(features, labels, mode, params):
+
+    # Input Layer
+    print("Mode:", mode)
+    input_layer = tf.reshape(features, [-1, 28, 28, 1], name="image_input")
+
+    # Encoder
+    conv1_1 = tf.layers.Conv2D(16, (3, 3), activation='relu', padding='same')(input_layer)
+    pool1 = tf.layers.MaxPooling2D((2, 2), (2, 2), padding='same')(conv1_1)
+    conv1_2 = tf.layers.Conv2D(8, (3, 3), activation='relu', padding='same')(pool1)
+    pool2 = tf.layers.MaxPooling2D((2, 2), (2, 2), padding='same')(conv1_2)
+    conv1_3 = tf.layers.Conv2D(8, (3, 3), activation='relu', padding='same')(pool2)
+    h = tf.layers.MaxPooling2D((2, 2), (2, 2), padding='same', name='feature_map')(conv1_3)
+
+    # Print dimensionality of lowest level
+    _, height, width, depth = h.get_shape()
+    print("CNN with final feature maps:", height, "x", width, "x", depth)
+    print(height*width*depth, "total features")
+    
+    # Decoder
+    conv2_1 = tf.layers.Conv2D(8, (3, 3), activation='relu', padding='same')(h)
+    up1 = tf.keras.layers.UpSampling2D((2, 2))(conv2_1)
+    conv2_2 = tf.layers.Conv2D(8, (3, 3), activation='relu', padding='same')(up1)
+    up2 = tf.keras.layers.UpSampling2D((2, 2))(conv2_2)
+    conv2_3 = tf.layers.Conv2D(16, (3, 3), activation='relu')(up2)
+    up3 = tf.keras.layers.UpSampling2D((2, 2))(conv2_3)
+    reconstructed = tf.layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same', 
+                                     activity_regularizer=tf.nn.l2_loss,
+                                     name='reconstructed_image')(up3)
+
+    # Calculate Loss
+    loss = tf.losses.mean_squared_error(labels=input_layer,
+                                      predictions=reconstructed)
+                                   
+    # Put images in tensorboard
+    tf.summary.image(
+        "Image",
+        reconstructed,
+        max_outputs=18
+      )
+    
+    # Configure the Training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        # reducing e to 0.0001 midway through training increases accuracy
+        optimizer = tf.train.AdamOptimizer(epsilon=0.01)
+        train_op = optimizer.minimize(
+                    loss=loss,
+                    global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+        
+    
+    # EVAL stuff
+    # Add evaluation metrics (for EVAL mode)
+    eval_metric_ops = {
+      "RMSE": tf.metrics.root_mean_squared_error(
+          labels=input_layer, predictions=reconstructed)
+    }
+
+    return tf.estimator.EstimatorSpec(mode=mode,
+                                      loss=loss,
+                                      eval_metric_ops=eval_metric_ops)
---
+++ CNN/train.py
@@ -0,0 +1,80 @@
+# Imports
+import numpy as np
+import os
+import argparse
+import tensorflow as tf
+import data_processing.audio_processing as ap
+
+# Autoencoders
+from autoencoders import conv, vanilla
+
+# which model to use
+cnn_model = conv.encode
+
+tf.logging.set_verbosity(tf.logging.WARN)
+#DEBUG, INFO, WARN, ERROR, or FATAL
+
+#Argument parsing
+parser = argparse.ArgumentParser()
+parser.add_argument("output_name", help="Specify model output name")
+parser.add_argument("--steps", default=5000, help="Train to number of steps")
+args = parser.parse_args()
+num_steps = int(args.steps)
+
+CWD_PATH = os.getcwd()
+
+def main(unused_argv):
+
+    # Set up data
+    train_data = ap.nsynth("nsynth/nsynth-train/")
+  
+    # Define params for model
+    params = {}
+    params['num_labels'] = 1
+
+    # Estimator config to change frequency of ckpt files
+    estimator_config = tf.estimator.RunConfig(
+    save_checkpoints_secs = 10,  # Save checkpoints every 10 seconds
+    keep_checkpoint_max = 2)       # Retain the 2 most recent checkpoints.
+
+    # Create the Estimator
+    classifier = tf.estimator.Estimator(
+    model_fn=cnn_model,
+    model_dir=CWD_PATH+"/models/"+args.output_name,
+    config=estimator_config,
+    params=params)
+  
+    # Train
+    train_images = [0]
+    while len(train_images) > 0:
+  
+        batch = train_data.returnInstance(12800)
+        train_images = [ ap.plotSpectrumBW(image) for image in batch['fft'] ]
+        train_images = np.asarray(train_images, dtype=np.float16)
+        train_images *= 1/255.0
+  
+        train_input_fn = \
+               tf.estimator.inputs.numpy_input_fn(train_images,
+                                                  batch_size=128,
+                                                  num_epochs=1,
+                                                  shuffle=True,
+                                                  queue_capacity=1024,
+                                                  num_threads=2)
+
+        # Set up logging for predictions
+        #tensors_to_log = {"predictions": "image_output"}
+        #logging_hook = tf.train.LoggingTensorHook(
+        #    tensors=tensors_to_log, every_n_iter=50)
+          
+        # Train the model
+        classifier.train(
+            input_fn=train_input_fn,
+            steps=None)
+            
+        batch = train_data.returnInstance(12800)
+        train_images = [ ap.plotSpectrumBW(image) for image in batch['fft'] ]
+        train_images = np.asarray(train_images, dtype=np.float16)
+        train_images *= 1/255.0
+
+if __name__ == "__main__":
+  tf.app.run()
