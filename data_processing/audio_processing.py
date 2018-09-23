fe9022ade65690a93147017648e0bb17399e9d83
Sirebellum <joshua.herrera227@gmail.com>
2018-09-23 15:26:01 -0500

    Removed pre-emphasis

--- data_processing/audio_processing.py
+++ data_processing/audio_processing.py
@@ -21,7 +21,7 @@ def plotSpectrumBW(data):
     data = np.round(data).astype(np.uint8)
     
     # Slice to make even square
-    return data[0:112, 0:112]
+    return data
     
 # Plot spectrum picture
 def plotSpectrum(data):
@@ -87,19 +87,19 @@ def convertWav(input, \
         signal = signal[0:int(crop * sample_rate)] #crop to crop seconds default 4
 
     ###Pre-emphasize signal
-    PRE_EMPHASIS = 0.97
-    emphasized_signal = np.append(signal[0], signal[1:] - PRE_EMPHASIS * signal[:-1])
+    #PRE_EMPHASIS = 0.97
+    #emphasized_signal = np.append(signal[0], signal[1:] - PRE_EMPHASIS * signal[:-1])
 
     ###Framing
     frame_length, frame_step = frame_size * sample_rate, frame_stride * sample_rate  # Convert from seconds to samples
-    signal_length = len(emphasized_signal)
+    signal_length = len(signal)
     frame_length = int(round(frame_length))
     frame_step = int(round(frame_step))
     num_frames = int(np.ceil(float(np.abs(signal_length - frame_length)) / frame_step))  # Make sure that we have at least 1 frame
 
     pad_signal_length = num_frames * frame_step + frame_length
     z = np.zeros((pad_signal_length - signal_length))
-    pad_signal = np.append(emphasized_signal, z) # Pad Signal to make sure that all frames have equal number of samples without truncating any samples from the original signal
+    pad_signal = np.append(signal, z) # Pad Signal to make sure that all frames have equal number of samples without truncating any samples from the original signal
 
     indices = np.tile(np.arange(0, frame_length), (num_frames, 1)) + np.tile(np.arange(0, num_frames * frame_step, frame_step), (frame_length, 1)).T
     frames = pad_signal[indices.astype(np.int32, copy=False)]
@@ -237,12 +237,13 @@ def main():
     dir = "nsynth/nsynth-test/"
     dataset_test = nsynth(dir)
 
-    batch = dataset_test.returnInstance(15)
+    batch = dataset_test.returnInstance(100)
     images = [ plotSpectrumBW(image) for image in batch['fft'] ]
 
     #import ipdb; ipdb.set_trace()
-    for image in images:
-        img = Image.fromarray(image, 'L')
+    for x in range(0, len(images)):
+      if batch['instrument_family_str'][x] == "flute":
+        img = Image.fromarray(images[x], 'L')
         img.show()
 
 if __name__ == "__main__":
