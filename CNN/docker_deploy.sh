ad1b377e19c432977184093fe826228f276bf413
Sirebellum <joshua.herrera227@gmail.com>
2018-09-29 21:43:17 -0500

    Expose rest and gprc ports

--- CNN/docker_deploy.sh
+++ CNN/docker_deploy.sh
@@ -1,5 +1,12 @@
-docker run --runtime=nvidia -p 8501:8501 \
+model=$1
+if [ -z $1 ]
+then
+    echo "Please specify a model!"
+    exit
+fi
+
+docker run --runtime=nvidia -p 8500:8500 -p 8501:8501 \
 --mount type=bind,\
-source=$(pwd)/deploy/feedback,\
-target=/models/feedback \
--e MODEL_NAME=feedback -t tensorflow/serving:latest-gpu
+source=$(pwd)/deploy/$model,\
+target=/models/$model \
+-e MODEL_NAME=$model -t tensorflow/serving:latest-gpu
