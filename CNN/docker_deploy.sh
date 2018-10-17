70a36a20e763a865f9aad31364311b562e32e2aa
Sirebellum <joshua.herrera227@gmail.com>
2018-10-17 11:18:04 -0500

    Partial memory deployment

--- CNN/docker_deploy.sh
+++ CNN/docker_deploy.sh
@@ -9,4 +9,5 @@ docker run --rm -t -i --runtime=nvidia -p 8500:8500 -p 8501:8501 \
 --mount type=bind,\
 source=$(pwd)/deploy/$model,\
 target=/models/$model \
--e MODEL_NAME=$model -t tensorflow/serving:latest-gpu
+-e MODEL_NAME=$model \
+-t tensorflow/serving:latest-gpu --per_process_gpu_memory_fraction=0.4
